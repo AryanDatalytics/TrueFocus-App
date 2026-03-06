8b421ea53c0d09914b65b9073ed5785765d6fefc
Aryan Dixit <heyaryan890@gmail.com>
2026-03-06 22:22:22 +0530

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,53 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(TrueFocusApp());
+}
+
+class TrueFocusApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TrueFocus',
+      debugShowCheckedModeBanner: false,
+      home: DashboardScreen(),
+    );
+  }
+}
+
+class DashboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TrueFocus"),
+      ),
+
+      body: Padding(
+        padding: EdgeInsets.all(16),
+
+        child: Column(
+          children: [
+
+            Card(
+              child: ListTile(
+                title: Text("Today's Screen Time"),
+                subtitle: Text("2h 10m"),
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            Card(
+              child: ListTile(
+                title: Text("Most Used App"),
+                subtitle: Text("Instagram"),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
