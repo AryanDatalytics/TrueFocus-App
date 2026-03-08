b8f5d23f5b430f0fe7b70f15e8183561e5093d6c
Aryan Dixit <heyaryan890@gmail.com>
2026-03-08 22:26:24 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(TrueFocusApp());
@@ -10,44 +11,10 @@ class TrueFocusApp extends StatelessWidget {
     return MaterialApp(
       title: 'TrueFocus',
       debugShowCheckedModeBanner: false,
-      home: DashboardScreen(),
-    );
-  }
-}
-
-class DashboardScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("TrueFocus"),
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
       ),
-
-      body: Padding(
-        padding: EdgeInsets.all(16),
-
-        child: Column(
-          children: [
-
-            Card(
-              child: ListTile(
-                title: Text("Today's Screen Time"),
-                subtitle: Text("2h 10m"),
-              ),
-            ),
-
-            SizedBox(height: 20),
-
-            Card(
-              child: ListTile(
-                title: Text("Most Used App"),
-                subtitle: Text("Instagram"),
-              ),
-            ),
-
-          ],
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
