8613d680a43db990a4ba9cc2ba3309b3f745f2b5
Aryan Dixit <heyaryan890@gmail.com>
2026-03-08 22:24:23 +0530

    Update dashboard_screen.dart

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,1 +1,57 @@
+import 'package:flutter/material.dart';
 
+class DashboardScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text("TrueFocus Dashboard"),
+      ),
+
+      body: Padding(
+        padding: EdgeInsets.all(16),
+
+        child: Column(
+
+          children: [
+
+            Card(
+              elevation: 4,
+              child: ListTile(
+                leading: Icon(Icons.timer, size: 40),
+                title: Text("Today's Screen Time"),
+                subtitle: Text("2h 15m"),
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            Card(
+              elevation: 4,
+              child: ListTile(
+                leading: Icon(Icons.apps, size: 40),
+                title: Text("Most Used App"),
+                subtitle: Text("Instagram"),
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            Card(
+              elevation: 4,
+              child: ListTile(
+                leading: Icon(Icons.lock, size: 40),
+                title: Text("Apps Blocked Today"),
+                subtitle: Text("2 Apps"),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
