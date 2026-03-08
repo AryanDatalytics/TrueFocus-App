9a738e841047ecc1cff3dda22f4e5740ac302e99
Aryan Dixit <heyaryan890@gmail.com>
2026-03-08 22:23:58 +0530

    Update apps_screen.dart

--- lib/screens/apps_screen.dart
+++ lib/screens/apps_screen.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class AppsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Apps Usage Screen",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
