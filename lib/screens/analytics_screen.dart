da344a43792a0ef57f32d4482559037c4b34e4dd
Aryan Dixit <heyaryan890@gmail.com>
2026-03-08 22:23:27 +0530

    Update analytics_screen.dart

--- lib/screens/analytics_screen.dart
+++ lib/screens/analytics_screen.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class AnalyticsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Analytics Screen",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
